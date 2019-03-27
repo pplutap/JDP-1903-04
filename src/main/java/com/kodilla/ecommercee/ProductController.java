fbb6dc5cd7b09e02ebdb68bf3523a97655f216a0
unknown <s.medykowski@gmail.com>
2019-03-27 22:14:58 +0100

    JDP190304-1 ProductController

--- src/main/java/com/kodilla/ecommercee/ProductController.java
+++ src/main/java/com/kodilla/ecommercee/ProductController.java
@@ -1,4 +1,80 @@
 package com.kodilla.ecommercee;
 
+
+import com.kodilla.ecommercee.domain.ProductDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+
+/*
+lista wszystkich produktów
+pobranie pojedynczego produktu
+utworzenie produktu
+aktualizacja danych produktu
+usunięcie produktu
+ */
+
+@RestController
+@RequestMapping("/v1/product")
 public class ProductController {
+
+    ProductDto computer = new ProductDto(1L, "computer", "test");
+    ProductDto laptop = new ProductDto(2L, "laptop", "test");
+    List<ProductDto> products = new ArrayList<>();
+
+    private List<ProductDto> returnListProduct() {
+        if (products.isEmpty()) {
+            products.add(computer);
+            products.add(laptop);
+        }
+        return products;
+    }
+
+    @GetMapping(value = "getProducts")
+    public List<ProductDto> getProducts() {
+        return returnListProduct();
+    }
+
+    @GetMapping(value = "getProduct")
+    public ProductDto getProduct(@RequestParam("productId") long productId) {
+        returnListProduct();
+        for (int i = 0; i < products.size(); i++) {
+            if (products.get(i).getProductId().equals(productId)) {
+                return products.get(i);
+            }
+        }
+        return null;
+    }
+
+    @PostMapping(value = "createProduct", consumes = APPLICATION_JSON_VALUE)
+    public ProductDto createProduct(@RequestBody ProductDto productDto) {
+        products.add(productDto);
+        return productDto;
+    }
+
+    @PutMapping(value = "updateProduct")
+    public ProductDto updateTask(@RequestBody ProductDto productDto) {
+        Long productId = productDto.getProductId();
+        for (int i = 0; i < products.size(); i++) {
+            if (products.get(i).getProductId().equals(productId)) {
+                products.get(i).setName(productDto.getName());
+                products.get(i).setDescription(productDto.getDescription());
+            }
+        }
+        return productDto;
+    }
+
+    @DeleteMapping(value = "deleteProduct")
+    public void deleteTask(@RequestParam("productId") long productId) {
+        returnListProduct();
+        for (int i = 0; i < products.size(); i++) {
+            if (products.get(i).getProductId().equals(productId)) {
+                products.remove(i);
+            }
+        }
+    }
 }
---
+++ src/main/java/com/kodilla/ecommercee/domain/ProductDto.java
@@ -0,0 +1,26 @@
+package com.kodilla.ecommercee.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class ProductDto {
+    private  Long productId;
+    private  String name;
+    private  String description;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
