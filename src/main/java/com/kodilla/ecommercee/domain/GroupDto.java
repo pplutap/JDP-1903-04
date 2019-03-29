af4e25c50e57369467ad23e7ab2d3d1f19c3e135
bcaban <b.caban1996@gmail.com>
2019-03-29 08:25:20 +0100

    JDP190304-2 GroupController

---
+++ src/main/java/com/kodilla/ecommercee/domain/GroupDto.java
@@ -0,0 +1,19 @@
+package com.kodilla.ecommercee.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class GroupDto {
+    private Long groupId;
+    private String groupName;
+    private List<ProductDto> productsInGroup = new ArrayList<>();
+}
