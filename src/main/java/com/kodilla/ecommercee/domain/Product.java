32dbb1aa018a0f69c531ddad58309c2908c67f14
dkoplenski <dkoplenski@gmail.com>
2019-03-29 21:36:21 +0100

    Dodana metoda dodawania zamowienia na podstawie koszyka, usuniety atrybut Cart z controllera

--- src/main/java/com/kodilla/ecommercee/domain/Product.java
+++ src/main/java/com/kodilla/ecommercee/domain/Product.java
@@ -3,15 +3,15 @@ package com.kodilla.ecommercee.domain;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Setter
 public class Product {
 
-    private Long productId;
-    private String productName;
-
-    public Product(String productName) {
-        this.productName = productName;
-    }
+    private  Long productId;
+    private  String name;
+    private  String description;
 }
--- src/main/java/com/kodilla/ecommercee/domain/ProductDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/ProductDto.java
@@ -3,13 +3,14 @@ package com.kodilla.ecommercee.domain;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
+@Setter
 public class ProductDto {
-
-    private Long productId;
-    private String productName;
-
+    private  Long productId;
+    private  String name;
+    private  String description;
 }
