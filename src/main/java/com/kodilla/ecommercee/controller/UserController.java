8a47a44e1e496764618e1532a6d811550011573f
DMizyn <dominik.mizyn@gmail.com>
2019-03-31 12:35:27 +0200

    Tworzenie nowego uzytkownika, zablokowanie uzytkownika, generowanie losowego klucza

---
+++ src/main/java/com/kodilla/ecommercee/controller/UserController.java
@@ -0,0 +1,45 @@
+package com.kodilla.ecommercee.controller;
+
+import com.kodilla.ecommercee.domain.UserDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RestController
+@RequestMapping("/v1/user")
+public class UserController {
+    private List<UserDto> users = new ArrayList<>();
+
+    @PostMapping(value = "createUser", consumes = APPLICATION_JSON_VALUE)
+    public void createUser(@RequestBody UserDto userDto) {
+        users.add(userDto);
+    }
+
+    @PutMapping(value = "badUser")
+    public UserDto badUser(@RequestBody Long id) {
+        UserDto user = getUserForSpecificID(id);
+        user.setStatus((long) 0);
+        return user;
+    }
+
+    @GetMapping(value = "getToken", consumes = APPLICATION_JSON_VALUE)
+    public Long getToken(@RequestBody UserDto userDto){
+        Random random = new Random();
+        Long randomLong = random.nextLong();
+        return randomLong;
+    }
+
+    private UserDto getUserForSpecificID(Long id){
+        for (UserDto user:users){
+            if (user.getId()==id){
+                return user;
+            }
+        }
+        return null;
+    }
+
+}
