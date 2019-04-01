7db6b308a4aac241c09777c1d619b73142c2f54e
dkoplenski <dkoplenski@gmail.com>
2019-04-01 17:17:26 +0200

    Poprawione zwracanie ProductDto przez CartDto

--- src/main/java/com/kodilla/ecommercee/domain/CartDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/CartDto.java
@@ -7,17 +7,17 @@ import lombok.NoArgsConstructor;
 import java.util.ArrayList;
 import java.util.List;
 
-@AllArgsConstructor
 @NoArgsConstructor
 @Getter
 public class CartDto {
 
     private Long cartId;
     private String cartName;
-    private List<Product> products = new ArrayList<>();
+    private List<ProductDto> products = new ArrayList<>();
 
-    public CartDto(String cartName) {
+    public CartDto(Long cartId, String cartName, List<ProductDto> products) {
+        this.cartId = cartId;
         this.cartName = cartName;
+        this.products = products;
     }
-
 }
--- src/main/java/com/kodilla/ecommercee/mapper/ProductMapper.java
+++ src/main/java/com/kodilla/ecommercee/mapper/ProductMapper.java
@@ -3,11 +3,25 @@ package com.kodilla.ecommercee.mapper;
 import com.kodilla.ecommercee.domain.Product;
 import com.kodilla.ecommercee.domain.ProductDto;
 import org.springframework.stereotype.Component;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class ProductMapper {
 
     public Product mapToProduct(final ProductDto productDto) {
         return new Product(productDto.getProductId(), productDto.getName(), productDto.getDescription());
+    }
+
+    public List<ProductDto> mapToProductDtoList(final List<Product> products) {
+        return products.stream()
+                .map(product -> new ProductDto(product.getProductId(), product.getName(), product.getDescription()))
+                .collect(Collectors.toList());
+    }
+
+    public List<Product> mapToProductList(final List<ProductDto> productDtoList) {
+        return productDtoList.stream()
+                .map(productDto -> new Product(productDto.getProductId(), productDto.getName(), productDto.getDescription()))
+                .collect(Collectors.toList());
     }
 }
