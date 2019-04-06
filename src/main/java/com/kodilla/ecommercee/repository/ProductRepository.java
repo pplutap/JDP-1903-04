3fc5084e9428456268bd43b8750d2a0072838709
unknown <s.medykowski@gmail.com>
2019-04-06 03:00:11 +0200

    ProductRepository

---
+++ src/main/java/com/kodilla/ecommercee/repository/ProductRepository.java
@@ -0,0 +1,21 @@
+package com.kodilla.ecommercee.repository;
+
+import com.kodilla.ecommercee.domain.products.Product;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends CrudRepository<Product, Long> {
+    @Override
+    List<Product> findAll();
+
+    @Override
+    Product save(Product product);
+
+    void deleteById(Long id);
+
+    Optional<Product> findById(Long id);
+}
