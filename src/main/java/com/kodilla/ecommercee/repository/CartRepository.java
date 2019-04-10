c22c9abc297f6f6bd45c300d03411cf26eeeb747
SlawomirJablonski <forest19@interia.pl>
2019-04-10 10:03:36 +0200

    JDP190304-17_encjaProductTesty_zamiana rozszerzenie na JpaRepository w repozytoriach

--- src/main/java/com/kodilla/ecommercee/repository/CartRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/CartRepository.java
@@ -1,16 +1,10 @@
 package com.kodilla.ecommercee.repository;
 
 import com.kodilla.ecommercee.domain.carts.Cart;
-import com.kodilla.ecommercee.domain.products.Product;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-public interface CartRepository extends CrudRepository<Cart, Long> {
-
-    @Override
-    List<Cart> findAll();
+public interface CartRepository extends JpaRepository<Cart, Long> {
 
 }
