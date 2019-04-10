730815b4f3bae2db9b47192e4f137745faf0d2c1
unknown <s.medykowski@gmail.com>
2019-04-10 19:05:20 +0200

    order to fix circle

--- src/main/java/com/kodilla/ecommercee/domain/orders/Order.java
+++ src/main/java/com/kodilla/ecommercee/domain/orders/Order.java
@@ -1,18 +1,18 @@
 package com.kodilla.ecommercee.domain.orders;
 
 import com.kodilla.ecommercee.domain.users.User;
-import lombok.AllArgsConstructor;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
-@AllArgsConstructor
+
 @NoArgsConstructor
 @Setter
 @Getter
@@ -36,7 +36,7 @@ public class Order {
             fetch = FetchType.LAZY
     )
     private List<Item> items = new ArrayList<>();
-    @Column(name = "paid")
+    @Column(name = "is_paid")
     private boolean paid;
 
     public Order(LocalDate date, boolean paid) {
