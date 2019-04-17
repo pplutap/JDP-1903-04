5d72c0d2c098fa8bda3a3f7ec0e1716315669418
SlawomirJablonski <forest19@interia.pl>
2019-04-17 18:23:22 +0200

    JDP190304-42 Import produktów z pliku csv

--- src/main/java/com/kodilla/ecommercee/controller/ProductController.java
+++ src/main/java/com/kodilla/ecommercee/controller/ProductController.java
@@ -5,9 +5,16 @@ import com.kodilla.ecommercee.domain.products.ProductDto;
 import com.kodilla.ecommercee.exceptions.ProductNotFoundException;
 import com.kodilla.ecommercee.mapper.ProductMapper;
 import com.kodilla.ecommercee.service.ProductService;
+import com.opencsv.CSVReader;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -55,5 +62,31 @@ public class ProductController {
     @DeleteMapping(value = "deleteProduct")
     public void deleteProduct(@RequestParam("productId") long productId) {
         productService.deleteProduct(productId);
+    }
+
+    @PostMapping(value = "addNewProducts")
+    public void addNewsFromCSV(@RequestParam String header)throws IOException {
+
+        List<List<String>> records = new ArrayList<>();
+
+        try (CSVReader csvReader = new CSVReader(new FileReader("resources/products.csv"));) {
+            String[] values = null;
+
+            if(header.equalsIgnoreCase("yes")){
+                csvReader.readNext();
+            }
+
+            while ((values = csvReader.readNext()) != null) {
+                records.add(Arrays.asList(values));
+            }
+        }catch (IOException e){
+
+        }
+
+        for (List<String> list:records) {
+            String[] array = list.get(0).split(";");
+
+            productService.saveProduct(new Product(array[0],array[1],new BigDecimal(array[2])));
+        }
     }
 }
